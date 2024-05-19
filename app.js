09fc728a8fb09bf7aa7e5984c880e4976e23ea88
carteraymond <craymond78@gmail.com>
2024-05-19 18:26:25 -0400

    added cors

--- app.js
+++ app.js
@@ -3,19 +3,13 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-
+const cors = require('cors');
 
 const port = process.env.PORT || 8080;
 const app = express();
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
+app.use(bodyParser.json()).use(cors()).use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
