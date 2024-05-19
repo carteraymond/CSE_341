54188f1bb94db4d3bf42557ce2fb980abfbb87ad
carteraymond <craymond78@gmail.com>
2024-05-19 12:09:53 -0400

    added route for swagger

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/contacts', require('./contacts'))
-
+router.use('/', require('./swagger'));
 module.exports = router;
